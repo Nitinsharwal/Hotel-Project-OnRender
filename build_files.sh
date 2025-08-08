1129c4b2db6a3822331c85214dfa01c60b14e9a2
Nitin <nitinparjapat94@gail.com>
2025-08-08 18:08:53 +0530

    Add

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
-# Exit immediately if a command fails
 set -e
 
-# Upgrade pip using python3
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
-
-# Install all dependencies from requirements.txt
 python3 -m pip install -r requirements.txt
 
-# Run Django collectstatic
 python3 manage.py collectstatic --noinput
 
-# Create the output folder for Vercel
 mkdir -p staticfiles_build
 cp -r static/* staticfiles_build/
